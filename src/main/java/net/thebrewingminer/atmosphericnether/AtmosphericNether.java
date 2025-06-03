26cd670ac4b745ec0db449cc79984f6971042577
aryanvirdi <asv2005@icloud.com>
2025-06-02 18:38:31 -0700

    Setting up environment. Set project to 1.19.2 and renaming applicable fields, directories, and classes.

---
+++ src/main/java/net/thebrewingminer/atmosphericnether/AtmosphericNether.java
@@ -0,0 +1,13 @@
+package net.thebrewingminer.atmosphericnether;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AtmosphericNether implements ModInitializer {
+	public static final String MOD_ID = "atmospheric_nether";
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	@Override
+	public void onInitialize() {}
+}
---
+++ src/main/java/net/thebrewingminer/atmosphericnether/AtmosphericNetherClient.java
@@ -0,0 +1,8 @@
+package net.thebrewingminer.atmosphericnether;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AtmosphericNetherClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
