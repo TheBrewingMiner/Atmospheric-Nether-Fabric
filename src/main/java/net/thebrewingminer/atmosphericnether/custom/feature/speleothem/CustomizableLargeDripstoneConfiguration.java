08c24f06624376ac0ecf96e7a30982f65d73fa5c
aryanvirdi <asv2005@icloud.com>
2025-06-07 16:07:37 -0700

    Minecraft expected a tag id, but JSON format is already established to use a plain string. Custom codec created to translate the string so that JSON format can stay stable across modloaders.

--- src/main/java/net/thebrewingminer/atmosphericnether/custom/feature/speleothem/CustomizableLargeDripstoneConfiguration.java
+++ src/main/java/net/thebrewingminer/atmosphericnether/custom/feature/speleothem/CustomizableLargeDripstoneConfiguration.java
@@ -1,11 +1,13 @@
 package net.thebrewingminer.atmosphericnether.custom.feature.speleothem;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.block.Block;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.floatprovider.FloatProvider;
 import net.minecraft.util.math.intprovider.IntProvider;
 import net.minecraft.world.gen.feature.LargeDripstoneFeatureConfig;
@@ -14,13 +16,22 @@ import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 public class CustomizableLargeDripstoneConfiguration extends LargeDripstoneFeatureConfig {
     public final BlockStateProvider block;
     public final TagKey<Block> baseTag;
+    public static final Codec<TagKey<Block>> BLOCK_TAG_CODEC = Codec.STRING.flatXmap(       // Custom codec to wrap string from JSON file to what MC expects.
+            idString -> {
+                try {
+                    Identifier id = new Identifier(idString);
+                    TagKey<Block> tagKey = TagKey.of(RegistryKeys.BLOCK, id);
+                    return DataResult.success(tagKey);
+                } catch (Exception e) {
+                    return DataResult.error("Invalid tag id: " + idString);
+                }
+            },
+            tagKey -> DataResult.success(tagKey.id().toString())
+    );
 
     public static final Codec<CustomizableLargeDripstoneConfiguration> CODEC = RecordCodecBuilder.create((instance) -> instance.group(
         BlockStateProvider.TYPE_CODEC.fieldOf("block").forGetter((config) -> config.block),
-        TagKey.codec(RegistryKeys.BLOCK)
-            .fieldOf("base_tag")
-            .orElse(BlockTags.BASE_STONE_OVERWORLD)
-            .forGetter(config -> config.baseTag),
+        BLOCK_TAG_CODEC.fieldOf("base_tag").orElse(BlockTags.BASE_STONE_OVERWORLD).forGetter(config -> config.baseTag),
         Codec.intRange(1, 512).fieldOf("floor_to_ceiling_search_range").orElse(30).forGetter((config) -> config.floorToCeilingSearchRange),
         IntProvider.createValidatingCodec(1, 60).fieldOf("column_radius").forGetter((config) -> config.columnRadius),
         FloatProvider.createValidatedCodec(0.0F, 20.0F).fieldOf("height_scale").forGetter((config) -> config.heightScale),
