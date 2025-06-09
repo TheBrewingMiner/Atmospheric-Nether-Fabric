5d330c72524a752ada3a2ef6930889ad62c5641a
aryanvirdi <asv2005@icloud.com>
2025-06-08 19:13:33 -0700

    Updated spawn location type.

--- src/main/java/net/thebrewingminer/atmosphericnether/custom/events/ZoglinSpawnHandler.java
+++ src/main/java/net/thebrewingminer/atmosphericnether/custom/events/ZoglinSpawnHandler.java
@@ -1,6 +1,7 @@
 package net.thebrewingminer.atmosphericnether.custom.events;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnLocationTypes;
 import net.minecraft.entity.SpawnRestriction;
 import net.minecraft.world.Heightmap;
 import net.thebrewingminer.atmosphericnether.custom.entity.ZoglinHelper;
@@ -9,7 +10,7 @@ public class ZoglinSpawnHandler {
     public static void registerSpawning(){
         SpawnRestriction.register(
                 EntityType.ZOGLIN,
-                SpawnRestriction.Location.ON_GROUND,
+                SpawnLocationTypes.ON_GROUND,
                 Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
                 ZoglinHelper::canSpawn
         );
